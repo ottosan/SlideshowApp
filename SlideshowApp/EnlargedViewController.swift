6e4254a5f777fd92ace134eb12848766edcb3f29
音石朋恵 <tomoe_otoishi@Tomoe-Otoishi.local>
2017-02-04 00:50:48 +0900

    スライドショーアプリ

---
+++ SlideshowApp/EnlargedViewController.swift
@@ -0,0 +1,42 @@
+//
+//  EnlargedViewController.swift
+//  SlideshowApp
+//
+//  Created by 音石朋恵 on 2017/02/03.
+//  Copyright © 2017年 tomoe.otoishi. All rights reserved.
+//
+
+import UIKit
+
+class EnlargedViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var slide:String = "slide01.jpg"
+    //var image: Int = 0
+    //let slideImages: [String] = ["slide01.jpg", "slide02.jpg", "slide03.jpg"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // 画像をUIImageViewに設定する.
+        imageView.image = UIImage(named: slide)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
